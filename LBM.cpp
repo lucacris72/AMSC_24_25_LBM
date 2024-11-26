a62d6718a6fa4edbd2c22de0599eb08bde230f2b
FabioGhattasPolimi <fabio.ghattas@mail.polimi.it>
2024-11-26 17:23:43 +0000

    Added basic files

---
+++ LBM.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void init_equilibrium()
+{
+    
+}
+
+void stream_collide_save()
+{
+    
+}
+
+void compute_flow_properties()
+{
+    
+}
+
+
+void save_scalar(const char* name, double *scalar, unsigned int n)
+{
+    
+}
